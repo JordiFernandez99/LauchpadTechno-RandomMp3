e024850521f2fc46a7b9bb06857e6e8850c3c4b2
JordiFernandez99 <56018053+JordiFernandez99@users.noreply.github.com>
2021-11-24 12:47:29 +0100

    Add files via upload

---
+++ js/codi.js
@@ -0,0 +1,284 @@
+
+function animacioBoto() {
+    document.getElementById("launchpad").style.backgroundColor = "black";
+}
+
+document.getElementById("boto1").onclick = playPauseAudio1;
+
+
+function playPauseAudio1() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica").paused) {
+        document.getElementById("musica").play();
+        document.getElementById("boto1").src = "img/Boto2.svg";
+    } else {
+        document.getElementById("musica").pause();
+        document.getElementById("boto1").src = "img/Boto1.svg";
+    }
+}
+
+
+document.getElementById("boto2").onclick = playPauseAudio2;
+
+
+function playPauseAudio2() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica1").paused) {
+        document.getElementById("musica1").play();
+        document.getElementById("boto2").src = "img/Boto3.svg";
+    } else {
+        document.getElementById("musica1").pause();
+        document.getElementById("boto2").src = "img/Boto2.svg";
+    }
+}
+
+
+document.getElementById("boto3").onclick = playPauseAudio3;
+
+
+function playPauseAudio3() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica2").paused) {
+        document.getElementById("musica2").play();
+        document.getElementById("boto3").src = "img/Boto4.svg";
+    } else {
+        document.getElementById("musica2").pause();
+        document.getElementById("boto3").src = "img/Boto3.svg";
+    }
+}
+
+
+document.getElementById("boto4").onclick = playPauseAudio4;
+
+
+function playPauseAudio4() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica3").paused) {
+        document.getElementById("musica3").play();
+        document.getElementById("boto4").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica3").pause();
+        document.getElementById("boto4").src = "img/Boto4.svg";
+    }
+}
+
+document.getElementById("boto5").onclick = playPauseAudio5;
+
+
+function playPauseAudio5() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica4").paused) {
+        document.getElementById("musica4").play();
+        document.getElementById("boto5").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica4").pause();
+        document.getElementById("boto5").src = "img/Boto4.svg";
+    }
+}
+
+document.getElementById("boto6").onclick = playPauseAudio6;
+
+
+function playPauseAudio6() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica5").paused) {
+        document.getElementById("musica5").play();
+        document.getElementById("boto6").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica5").pause();
+        document.getElementById("boto6").src = "img/Boto4.svg";
+    }
+}
+
+document.getElementById("boto7").onclick = playPauseAudio7;
+
+
+function playPauseAudio7() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica6").paused) {
+        document.getElementById("musica6").play();
+        document.getElementById("boto7").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica6").pause();
+        document.getElementById("boto7").src = "img/Boto4.svg";
+    }
+}
+
+
+document.getElementById("boto8").onclick = playPauseAudio8;
+
+
+function playPauseAudio8() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica7").paused) {
+        document.getElementById("musica7").play();
+        document.getElementById("boto8").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica7").pause();
+        document.getElementById("boto8").src = "img/Boto4.svg";
+    }
+}
+
+
+document.getElementById("boto9").onclick = playPauseAudio9;
+
+
+function playPauseAudio9() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica8").paused) {
+        document.getElementById("musica8").play();
+        document.getElementById("boto9").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica8").pause();
+        document.getElementById("boto9").src = "img/Boto4.svg";
+    }
+}
+
+document.getElementById("boto10").onclick = playPauseAudio10;
+
+
+function playPauseAudio10() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica9").paused) {
+        document.getElementById("musica9").play();
+        document.getElementById("boto10").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica9").pause();
+        document.getElementById("boto10").src = "img/Boto4.svg";
+    }
+}
+
+document.getElementById("boto11").onclick = playPauseAudio11;
+
+
+function playPauseAudio11() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica10").paused) {
+        document.getElementById("musica10").play();
+        document.getElementById("boto11").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica10").pause();
+        document.getElementById("boto11").src = "img/Boto4.svg";
+    }
+}
+
+document.getElementById("boto12").onclick = playPauseAudio12;
+
+
+function playPauseAudio12() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica11").paused) {
+        document.getElementById("musica11").play();
+        document.getElementById("boto12").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica11").pause();
+        document.getElementById("boto12").src = "img/Boto4.svg";
+    }
+}
+
+document.getElementById("boto13").onclick = playPauseAudio13;
+
+
+function playPauseAudio13() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica12").paused) {
+        document.getElementById("musica12").play();
+        document.getElementById("boto13").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica12").pause();
+        document.getElementById("boto13").src = "img/Boto4.svg";
+    }
+}
+
+
+document.getElementById("boto14").onclick = playPauseAudio14;
+
+
+function playPauseAudio14() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica13").paused) {
+        document.getElementById("musica13").play();
+        document.getElementById("boto14").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica13").pause();
+        document.getElementById("boto14").src = "img/Boto4.svg";
+    }
+}
+
+
+document.getElementById("boto15").onclick = playPauseAudio15;
+
+
+function playPauseAudio15() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica14").paused) {
+        document.getElementById("musica14").play();
+        document.getElementById("boto15").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica14").pause();
+        document.getElementById("boto15").src = "img/Boto4.svg";
+    }
+}
+
+
+document.getElementById("boto16").onclick = playPauseAudio16;
+
+
+function playPauseAudio16() {
+    // document.getElementById("musica").muted = false;
+
+    if (document.getElementById("musica15").paused) {
+        document.getElementById("musica15").play();
+        document.getElementById("boto16").src = "img/Boto5.svg";
+    } else {
+        document.getElementById("musica15").pause();
+        document.getElementById("boto16").src = "img/Boto4.svg";
+    }
+}
+
+
+document.getElementById("sliderVol").oninput = canviaVol;
+
+function canviaVol() {
+    //Assignem el valor de l'slader al volum dereproducció
+    document.getElementById("musica").volume =
+        document.getElementById("sliderVol").value;
+};
+
+//Quan carreguem la pàgina assignem a l'slider el valor que tingui el volum
+document.getElementById("sliderVol").value =
+    document.getElementById("musica").volume;
+
+
+
+
+
+
+document.getElementById("sliderDur").max =
+    document.getElementById("musica").duration;
+
+
+//Fem que quan es toqui l'slider s'executi la funció canviaTemps
+document.getElementById("sliderDur").oninput = canviaTemps;
+
+function canviaTemps() {
+    //Assignem el valor de l'slader al volum dereproducció
+    document.getElementById("musica").currentTime =
+        document.getElementById("sliderDur").value;
+};
+
